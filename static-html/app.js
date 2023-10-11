19742bd42025079b6fd182f8e4ffeaad1d99f73d
ashish-pro <ashishbit017@outlook.com>
2023-10-11 18:47:59 +0530

    modification

--- static-html/app.js
+++ static-html/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import process from 'process'
+import path from 'path';
 
 
 
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.sendfile('views/login.html')
+    res.sendFile(path.join(process.cwd(),'views','index.html'))
 })
 
 app.listen(3000)
